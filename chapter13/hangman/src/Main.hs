ed855539d874f2435291b295299e7059f1f2b72c
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-02-12 02:18:43 +0100

    Add hangman dependencies

--- chapter13/hangman/src/Main.hs
+++ chapter13/hangman/src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import Control.Monad (forever) -- [1]
+import Data.Char (toLower) -- [2]
+import Data.Maybe (isJust) -- [3]
+import Data.List (intersperse) -- [4]
+import System.Exit (exitSuccess) -- [5]
+import System.Random (randomRIO) -- [6]
+
 main :: IO ()
 main = do
   putStrLn "hello world"
