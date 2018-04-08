9df81953f18f963a9e5ac2b56567ef9e60dc13bc
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-04-08 11:20:27 +0200

    Make ini file code more readable with MaybeT

--- chapter29/src/ConfigDirectories.hs
+++ chapter29/src/ConfigDirectories.hs
@@ -1,26 +1,27 @@
 module ConfigDirectories where
 
+import           Control.Monad.Trans.Maybe
 import           Data.Ini
 import           Data.List
-import qualified Data.Map             as M
+import qualified Data.Map                  as M
 import           System.Directory
 import           Text.Trifecta.Parser
 
 isIni :: FilePath -> Bool
 isIni = isSuffixOf ".ini"
 
-createConfigs :: [FilePath] -> IO (Maybe (M.Map FilePath Config))
+createConfigs :: [FilePath] -> MaybeT IO (M.Map FilePath Config)
 createConfigs paths = do
-  configs <- sequence <$> traverse createConfig paths
-  let pairs = (zip paths) <$> configs
-  return $ M.fromList <$> pairs
+  configs <- traverse createConfig paths
+  let pairs = zip paths configs
+  return $ M.fromList pairs
 
-createConfig :: FilePath -> IO (Maybe Config)
-createConfig = parseFromFile parseIni
+createConfig :: FilePath -> MaybeT IO Config
+createConfig = MaybeT . parseFromFile parseIni
 
 main :: IO ()
 main = do
   dirs <- listDirectory "."
   let iniPaths = filter isIni dirs
-  configs <- createConfigs iniPaths
+  configs <- runMaybeT $ createConfigs iniPaths
   print configs
